2848b74a56d04b60c6a1e0204037c8189fa31274
AndreaMiranda <martinezmirandandre2@gmail.com>
2026-08-14 22:02:37 -0400

    Actualiza caché para nueva frase

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "chino-basico-v3";
+const CACHE = "chino-basico-v4";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=3",
-  "./app.js?v=3",
+  "./app.js?v=4",
   "./manifest.webmanifest",
   "./icons/icon.svg"
 ];
